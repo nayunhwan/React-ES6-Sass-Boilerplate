52195a89efee3c3ef1910b0b6204597136b75609
nayunhwan <nayunhwan.dev@gmail.com>
2017-02-10 01:49:05 +0900

    Remove Pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 // React Common Modules
 import React, { Component } from 'react';
 // React Router
-import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 // Material UI Provider for React
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 // Own Modules
-import {LandingPage, SplashPage, DefaultPage} from './Pages/';
+import { DefaultPage } from './Pages/';
 
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
@@ -20,12 +20,6 @@ class App extends Component {
       <MuiThemeProvider>
         <Router history={browserHistory}>
           <Route path="/">
-            <IndexRoute component={LandingPage}/>
-          </Route>
-          <Route path="/splash">
-            <IndexRoute component={SplashPage}/>
-          </Route>
-          <Route path="/test">
             <IndexRoute component={DefaultPage}/>
           </Route>
         </Router>
--- src/Pages/DefaultPage/DefaultPage.js
+++ src/Pages/DefaultPage/DefaultPage.js
@@ -4,25 +4,9 @@
 
 import React, {Component} from 'react';
 
-import AppBar from 'material-ui/AppBar';
-import FontIcon from 'material-ui/FontIcon';
-import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
-import Paper from 'material-ui/Paper';
-import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
-
-const recentsIcon = <FontIcon className="material-icons">restore</FontIcon>;
-const favoritesIcon = <FontIcon className="material-icons">favorite</FontIcon>;
-const nearbyIcon = <IconLocationOn />;
-
-
-
 class DefaultPage extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      selectedIndex: 0
-    }
 
     this.select = (index) => {this.setState({selectedIndex: index})}
   }
@@ -30,25 +14,7 @@ class DefaultPage extends Component {
   render() {
     return (
       <div>
-        <AppBar
-          title="Test"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-        />
-
-        <BottomNavigation className="bottom-nav" selectedIndex={this.state.selectedIndex}>
-          <BottomNavigationItem
-            label="Recents"
-            icon={recentsIcon}
-          />
-          <BottomNavigationItem
-            label="Favorites"
-            icon={favoritesIcon}
-          />
-          <BottomNavigationItem
-            label="Nearby"
-            icon={nearbyIcon}
-          />
-        </BottomNavigation>
+        This is Skeleton Page
       </div>
     );
   }
