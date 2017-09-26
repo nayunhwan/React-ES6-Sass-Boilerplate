14d51e517d12055403a0b105ce0678c1e2b0b08c
nayunhwan <nayunhwan.dev@gmail.com>
2017-09-26 14:22:28 +0900

    Adjust indent

--- src/Pages/_DefaultPage/DefaultPage.js
+++ src/Pages/_DefaultPage/DefaultPage.js
@@ -1,21 +1,20 @@
-
 // This Page is Skeleton of React Structure for Web Development
 // If you want to make other page, Copy and Refactor this page.
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class DefaultPage extends Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return (
-      <div>
-        This is Default Page
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				This is Default Page
+			</div>
+		);
+	}
 }
 
-export default DefaultPage
+export default DefaultPage;
