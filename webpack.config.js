7157d4f40d6cd91cb551b5e4ac670dfe034165bc
Yunhwan Na <nayunhwan.dev@gmail.com>
2018-05-16 00:07:10 +0900

    Update modules

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ module.exports = {
   entry: ["babel-polyfill", "./src/index.js"],
 
   output: {
-    path: __dirname + "/public/",
+    path: __dirname + "/build/",
     filename: "bundle.js"
   },
 
   devServer: {
     inline: true,
     port: 7777,
-    contentBase: __dirname + "/public/",
+    contentBase: __dirname + "/build/",
     historyApiFallback: true
   },
   devtool: "source-map",
@@ -25,13 +25,26 @@ module.exports = {
         }
       },
       {
-        test: /\.scss$/,
+        test: /\.s?css$/,
         loaders: ["style-loader", "css-loader", "sass-loader"],
-        exclude: /node_modules/
+        options: {
+          modules: true,
+          minimize: true,
+          localIdentName: "[name]__[local]__[hash:base64:3]",
+          includePaths: ["node_modules", "src", "."]
+        }
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loader: "file-loader?name=/public/icons/[name].[ext]"
+      },
+      {
+        test: /\.json$/,
+        loader: "json-loader"
+      },
+      {
+        test: /\.(jpe?g|png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader"
       }
     ]
   }
